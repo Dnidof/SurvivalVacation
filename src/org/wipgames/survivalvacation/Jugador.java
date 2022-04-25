5ccb4991084c370f4e1abac4cf789f0c0c36f1e5
Usuario <Usuario@DESKTOP-K63FRKR>
2022-04-25 13:10:19 +0200

    Merge branch 'main' of https://github.com/Dnidof/SurvivalVacation.git into main
    he creado todos los metodos constructora y atributos de Jugador

--- src/org/wipgames/survivalvacation/Jugador.java
+++ src/org/wipgames/survivalvacation/Jugador.java
@@ -2,4 +2,37 @@ package org.wipgames.survivalvacation;
 
 public class Jugador {
 
+	
+	private int salud;
+	private int hambre;
+	private int sed;
+	private int cordura;
+	private static Jugador miJugador;
+	
+	private Jugador() {
+		
+	}
+	
+	public static Jugador getJugador() {
+		if (miJugador==null) {miJugador = new Jugador();}
+		return miJugador;
+			
+	}
+	
+	
+	public void imprimirEstadisticas() {
+		
+	}
+	
+	public void actualizarEstadisticas(int pSalud,int pHambre,int pSed,int pCordura) {
+		
+	}
+	
+	public void resetearJugador() {
+		
+	}
+	
+	public boolean sigueVivo() {
+		
+	}
 }
