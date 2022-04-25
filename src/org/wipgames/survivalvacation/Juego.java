d834013c7e0146c8eb6d11175e264c77196a6932
Usuario <Usuario@DESKTOP-K63FRKR>
2022-04-25 12:58:18 +0200

    JUego inicio

--- src/org/wipgames/survivalvacation/Juego.java
+++ src/org/wipgames/survivalvacation/Juego.java
@@ -1,10 +1,28 @@
 package org.wipgames.survivalvacation;
 
 public class Juego {
+	
+	private static Juego miJuego;
+	private boolean finPartida;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
 
+	
+	public void partida() {
+		
+		
+		
+		
+		
+	}
+	
+	
+	private void finPartida() {
+		
+		
+		
+	}
 }
