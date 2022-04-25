748a114c4d27d9e20658a67e1ff5861049f64208
Diego <nidofonsecadiego@gmail.com>
2022-04-25 13:35:38 +0200

    Arreglar errores

--- src/org/wipgames/survivalvacation/EventoConsecutivo.java
+++ src/org/wipgames/survivalvacation/EventoConsecutivo.java
@@ -1,7 +1,9 @@
 package org.wipgames.survivalvacation;
 
+import java.util.ArrayList;
+
 public class EventoConsecutivo extends Evento {
-	private Arraylist <EventoUnico> lista;
+	private ArrayList <EventoUnico> lista;
 
 	public EventoConsecutivo(int pId, String pEnunciado, Opcion pOpcion1, Opcion pOpcion2, Opcion pOpcion3) {
 		super(pId,pEnunciado,pOpcion1,pOpcion2,pOpcion3);
--- src/org/wipgames/survivalvacation/Objeto.java
+++ src/org/wipgames/survivalvacation/Objeto.java
@@ -1,30 +1,5 @@
 package org.wipgames.survivalvacation;
 
 public class Objeto {
-
 	
-	private String nombre;
-	private int idObjeto;
-	private int cantidad;
-	
-	public Objeto(String pNombre, int pIdObjeto, int pCantidad) {
-		
-		this.nombre=pNombre;
-		this.idObjeto=pObjeto;
-		this.cantidad=pCantidad;
-
-	}
-	
-	public boolean tieneEsteId(int pId) {
-		
-	}
-		
-	public void imprimirObjeto() {
-		
-	}
-	
-	public void actualizarCantidad(int pCantidad) {
-		
-	}
-		
 }
