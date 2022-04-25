49a2d80cfd8259d8f50f6b61f175991b04aa3474
imano <imano@DESKTOP-NTMBH2C>
2022-04-25 13:07:56 +0200

    Base de eventoConsecutivo

--- src/org/wipgames/survivalvacation/EventoConsecutivo.java
+++ src/org/wipgames/survivalvacation/EventoConsecutivo.java
@@ -1,5 +1,15 @@
 package org.wipgames.survivalvacation;
 
-public class EventoConsecutivo {
+public class EventoConsecutivo extends Evento {
+	private Arraylist <EventoUnico> lista;
 	
-}//yh
+	public EventoConsecutivo(int pId, String pEnunciado, Opcion pOpcion1, Opcion pOpcion2, Opcion pOpcion3) {
+		super(pId,pEnunciado,pOpcion1,pOpcion2,pOpcion3);
+		this.lista= new ArrayList <EventoUnico>();
+	}
+	
+	public void ejecutarEvento() {
+		
+	}
+}
+
