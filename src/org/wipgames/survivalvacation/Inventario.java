63641ec6b56016425bb4f988aef13116563384c8
Usuario <Usuario@DESKTOP-K63FRKR>
2022-04-25 13:47:54 +0200

    He creadeo todos los metodos de Inventario

--- src/org/wipgames/survivalvacation/Inventario.java
+++ src/org/wipgames/survivalvacation/Inventario.java
@@ -17,4 +17,40 @@ public class Inventario {
     	
     	return this.lista.iterator();
     }
+    
+    public void añadirObjeto(Objeto pObjeto) {
+    	
+    }
+    
+    public void eliminarObjeto() {
+    	
+    }
+    
+    public boolean tieneEsteObjeto(Objeto pObjeto) {
+    	
+    }
+    
+    public void imprimirInventario() {
+    	
+    }
+    
+    public void barco() {
+    	
+    }
+    
+    public void cargarObjetos() {
+    	
+    }
+    
+    public void resetear() {
+    	
+    }
+    
+    private Objeto buscarObjetoPorId(int pId) {
+    	
+    }
+    
+    public void actualizarObjetoPorId(int pIdObjeto, int pUnidades) {
+    	
+    }
 }
