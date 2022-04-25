ca8d4c551de845568940a6ddc3639515d3e112aa
Usuario <Usuario@DESKTOP-K63FRKR>
2022-04-25 13:18:56 +0200

    Objeto con todos sus metodos constructora y atributos

--- src/org/wipgames/survivalvacation/Objeto.java
+++ src/org/wipgames/survivalvacation/Objeto.java
@@ -2,4 +2,21 @@ package org.wipgames.survivalvacation;
 
 public class Objeto {
 
+	
+	private String nombre;
+	private int idObjeto;
+	private int cantidad;
+	
+	public boolean tieneEsteId(int pId) {
+		
+	}
+		
+	public void imprimirObjeto() {
+		
+	}
+	
+	public void actualizarCantidad(int pCantidad) {
+		
+	}
+		
 }
