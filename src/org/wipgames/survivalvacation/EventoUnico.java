bf5b2a4e5f6c64dad4df840c3e3f473f10356ed4
imano <imano@DESKTOP-NTMBH2C>
2022-04-25 13:20:49 +0200

    base de eventoUnico

--- src/org/wipgames/survivalvacation/EventoUnico.java
+++ src/org/wipgames/survivalvacation/EventoUnico.java
@@ -1,8 +1,13 @@
 package org.wipgames.survivalvacation;
 
 public class EventoUnico extends Evento{
+	
 	public EventoUnico(int pId, String pEnunciado, Opcion pOpcion1, Opcion pOpcion2, Opcion pOpcion3) {
 		super(pId,pEnunciado,pOpcion1,pOpcion2,pOpcion3);
+	}
+	
+	public void ejecutarEvento() {
+		
 	}
 
 }
