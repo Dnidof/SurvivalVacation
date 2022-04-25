a546f12aebdbc9f22b653e8a7a6f63c0346597f2
Diego <nidofonsecadiego@gmail.com>
2022-04-25 13:55:18 +0200

    Objeto

--- src/org/wipgames/survivalvacation/Objeto.java
+++ src/org/wipgames/survivalvacation/Objeto.java
@@ -1,5 +1,30 @@
 package org.wipgames.survivalvacation;
 
 public class Objeto {
+
 	
+	private String nombre;
+	private int idObjeto;
+	private int cantidad;
+	
+	public Objeto(String pNombre, int pIdObjeto, int pCantidad) {
+		
+		this.nombre=pNombre;
+		this.idObjeto=pIdObjeto;
+		this.cantidad=pCantidad;
+
+	}
+	
+	public boolean tieneEsteId(int pId) {
+		
+	}
+		
+	public void imprimirObjeto() {
+		
+	}
+	
+	public void actualizarCantidad(int pCantidad) {
+		
+	}
+		
 }
