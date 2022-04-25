74202350e7fdc5da05e66a43008daac60bb6896b
amuno <amuno@192.168.1.136>
2022-04-25 13:26:19 +0200

    Inventario MAE

--- src/org/wipgames/survivalvacation/Inventario.java
+++ src/org/wipgames/survivalvacation/Inventario.java
@@ -1,5 +1,15 @@
 package org.wipgames.survivalvacation;
 
 public class Inventario {
-
+	
+	private ArrayList<Objeto> lista;
+	private static Inventario miInventario= new Inventario();
+	
+	private Inventario() {
+		this.lista = new ArrayList<Objeto>();
+	}
+	
+	public static Inventario getInventario() {
+		return miInventario;
+	}
 }
