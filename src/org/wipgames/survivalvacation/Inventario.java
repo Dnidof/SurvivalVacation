c86124dac18add5ae3785d6b67a8701d1e44d02f
amuno <amuno@192.168.1.136>
2022-04-25 13:34:04 +0200

    MAE Inventario más eficiente

--- src/org/wipgames/survivalvacation/Inventario.java
+++ src/org/wipgames/survivalvacation/Inventario.java
@@ -3,13 +3,13 @@ package org.wipgames.survivalvacation;
 public class Inventario {
 	
 	private ArrayList<Objeto> lista;
-	private static Inventario miInventario= new Inventario();
+	private static Inventario miInventario;
 	
 	private Inventario() {
 		this.lista = new ArrayList<Objeto>();
 	}
-	
 	public static Inventario getInventario() {
+		if(miInventario == null) miInventario = new Inventario();
 		return miInventario;
 	}
 }
