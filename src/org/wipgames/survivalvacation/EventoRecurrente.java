a64c380c41d6e5c440d34fe33e811acd598ea6f3
imano <imano@DESKTOP-NTMBH2C>
2022-04-25 13:30:27 +0200

    base de eventoRecurrente

--- src/org/wipgames/survivalvacation/EventoRecurrente.java
+++ src/org/wipgames/survivalvacation/EventoRecurrente.java
@@ -1,5 +1,15 @@
 package org.wipgames.survivalvacation;
 
 public class EventoRecurrente {
+	private int idEvento;
+	
+	public EventoRecurrente(int pId, String pEnunciado, Opcion pOpcion1, Opcion pOpcion2, Opcion pOpcion3, int idEvento) {
+		super(pId,pEnunciado,pOpcion1,pOpcion2,pOpcion3);
+		this.idEvento= pIdEvento;
+	}
+	
+	public void ejecutarEvento() {
+		
+	}
 
 }
