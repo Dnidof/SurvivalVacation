a70a347df539386caa3ba56a1b3a8a212f3373d0
Usuario <Usuario@DESKTOP-K63FRKR>
2022-04-26 19:48:54 +0200

    retoque

--- src/org/wipgames/survivalvacation/Juego.java
+++ src/org/wipgames/survivalvacation/Juego.java
@@ -3,7 +3,6 @@ package org.wipgames.survivalvacation;
 public class Juego {
 	
 	private static Juego miJuego;
-	private boolean finPartida;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
