fac1c715ec13a9266044aa8ea3573fd1a5972133
imano <imano@DESKTOP-NTMBH2C>
2022-04-27 18:12:50 +0200

    EventoRecurrente arreglo

--- src/org/wipgames/survivalvacation/EventoRecurrente.java
+++ src/org/wipgames/survivalvacation/EventoRecurrente.java
@@ -1,12 +1,9 @@
 package org.wipgames.survivalvacation;
 
 public class EventoRecurrente extends Evento {
-	//a lo mejor no necesitamos este atributo ya que lo vamos a hacer por posición del ArrayList
-	private int idEvento=0;
 	
-	public EventoRecurrente(int pId, String pEnunciado, Opcion pOpcion1, Opcion pOpcion2, Opcion pOpcion3, int idEvento) {
-		super(pId,pEnunciado,pOpcion1,pOpcion2,pOpcion3);
-		this.idEvento= idEvento;
+	public EventoRecurrente(int pId, String pEnunciado, Opcion pOpcion1, Opcion pOpcion2, Opcion pOpcion3) {
+		super(0,pEnunciado,pOpcion1,pOpcion2,pOpcion3);
 	}
 	
 	public void ejecutarEvento() {
