dbcd05a6a4668ae73089b9a71a133cca99e71ac5
Usuario <Usuario@DESKTOP-K63FRKR>
2022-04-27 19:12:24 +0200

    Inventario casi terminado

--- src/org/wipgames/survivalvacation/Inventario.java
+++ src/org/wipgames/survivalvacation/Inventario.java
@@ -26,7 +26,15 @@ public class Inventario {
     }
   
     public boolean tieneEsteObjeto(Objeto pObjeto) {
-    	return this.lista.contains(pObjeto);
+    	Iterator<Objeto> itr = this.getIterador();
+    	Objeto unObjeto = null;
+    	boolean lotTiene=false;
+    	while (itr.hasNext()) {
+    		unObjeto=itr.next();
+    		if (unObjeto.getCantidad()>=1) {
+    			loTiene=
+    		}
+    		}
     }
     
     public void imprimirInventario() {
@@ -47,10 +55,10 @@ public class Inventario {
     	System.out.println("1-   "  + this.buscarObjetoPorId(0).imprimirNombreObjeto() + "   -10 segundos");
     	System.out.println("2-   "  + this.buscarObjetoPorId(1).imprimirNombreObjeto() + "   -10 segundos");
     	System.out.println("3-   "  + this.buscarObjetoPorId(2).imprimirNombreObjeto() + "   -10 segundos");
-    	System.out.println("4-   "  + this.buscarObjetoPorId(3).imprimirNombreObjeto() + "   -x  segundos");
-    	System.out.println("5-   "  + this.buscarObjetoPorId(4).imprimirNombreObjeto() + "   -x  segundos");
-    	System.out.println("6-   "  + this.buscarObjetoPorId(5).imprimirNombreObjeto() + "   -x  segundos");
-    	System.out.println("7-   "  + this.buscarObjetoPorId(6).imprimirNombreObjeto() + "   -x  segundos");
+    	System.out.println("4-   "  + this.buscarObjetoPorId(3).imprimirNombreObjeto() + "   -20  segundos");
+    	System.out.println("5-   "  + this.buscarObjetoPorId(4).imprimirNombreObjeto() + "   -30  segundos");
+    	System.out.println("6-   "  + this.buscarObjetoPorId(5).imprimirNombreObjeto() + "   -20  segundos");
+    	System.out.println("7-   "  + this.buscarObjetoPorId(6).imprimirNombreObjeto() + "   -40  segundos");
     	int opcionElegida =Teclado.getMiTeclado().leerOpcion();
     	while (segundosRestantes>=0) {
     		if(opcionElegida==1 && segundosRestantes>=10) {
@@ -66,31 +74,68 @@ public class Inventario {
     			segundosRestantes= segundosRestantes - 10;
     		}
     		
-    		else if(opcionElegida==4 && segundosRestantes>=x) {
+    		else if(opcionElegida==4 && segundosRestantes>=20) {
     			this.buscarObjetoPorId(3).actualizarCantidad(1);
-    			segundosRestantes= segundosRestantes - x;
+    			segundosRestantes= segundosRestantes - 20;
     		}
-    		else if(opcionElegida==5 && segundosRestantes>=x) {
+    		else if(opcionElegida==5 && segundosRestantes>=30) {
     			this.buscarObjetoPorId(4).actualizarCantidad(1);
-    			segundosRestantes= segundosRestantes - x;
+    			segundosRestantes= segundosRestantes - 30;
     		}
     		
-    		else if(opcionElegida==6 && segundosRestantes>=x) {
+    		else if(opcionElegida==6 && segundosRestantes>=20) {
     			this.buscarObjetoPorId(5).actualizarCantidad(1);
-    			segundosRestantes= segundosRestantes - x;
+    			segundosRestantes= segundosRestantes - 20;
     		}
     		
-    		else if(opcionElegida==7 && segundosRestantes>=x) {
+    		else if(opcionElegida==7 && segundosRestantes>=40) {
     			this.buscarObjetoPorId(6).actualizarCantidad(1);
-    			segundosRestantes= segundosRestantes - x;
+    			segundosRestantes= segundosRestantes - 40;
     		}
+    		else if(opcionElegida==8 && segundosRestantes>=10) {
+    			this.buscarObjetoPorId(7).actualizarCantidad(1);
+    			segundosRestantes= segundosRestantes - 10;
+    		}
+    		else if(opcionElegida==9 && segundosRestantes>=30) {
+    			this.buscarObjetoPorId(8).actualizarCantidad(1);
+    			segundosRestantes= segundosRestantes - 30;
+    		}
+    		else {System.out.println("No tienes segundos suficientes para cojer este objeto prueba con uno que requiera de menos segundos");}
     		System.out.println("Te quedan " + segundosRestantes + " segundos" );
+    		}
+    		
     	}
     	
-    	}
+   
     
     public void cargarObjetos() {
-    	this.anadirObjeto(null);
+    	
+    	Objeto o1 = new Objeto("Vendas",0,2);
+    	this.anadirObjeto(o1);
+    	
+    	Objeto o2 = new Objeto("Lata de comida",1,2);
+    	this.anadirObjeto(o2);
+    	
+    	Objeto o3 = new Objeto("Botella de agua",2,2);
+    	this.anadirObjeto(o3);
+    	
+    	Objeto o4 = new Objeto("Palanca",3,0);
+    	this.anadirObjeto(o4);
+    	
+    	Objeto o5 = new Objeto("Espada",4,0);
+    	this.anadirObjeto(o5);
+    	
+    	Objeto o6 = new Objeto("Linterna",5,0);
+    	this.anadirObjeto(o6);
+    	
+    	Objeto o7 = new Objeto("Oro",6,0);
+    	this.anadirObjeto(o7);
+    	
+    	Objeto o8 = new Objeto("Llave Misteriosa",7,0);
+    	this.anadirObjeto(o8);
+    	
+    	Objeto o9 = new Objeto("Radio",8,0);
+    	this.anadirObjeto(o9);
     }
     
     public void resetear() {
