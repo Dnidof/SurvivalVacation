ea3d1b74ae9073e983125562ad50aeac8d8fef93
Usuario <Usuario@DESKTOP-K63FRKR>
2022-04-27 19:44:17 +0200

    arreglo del destrozo

--- src/org/wipgames/survivalvacation/EventoUnico.java
+++ src/org/wipgames/survivalvacation/EventoUnico.java
@@ -14,36 +14,13 @@ public class EventoUnico extends Evento{
 			
 			int opcion = Teclado.getMiTeclado().leerOpcion();
 			
-<<<<<<< HEAD
-			if(opcion==1 && this.getOpcion(3).cumpleRequisitoObjeto()) {
-				this.getOpcion(1).aplicarConsecuencias();
-				seHaRealizado=true;
-			}
-=======
 			if (opcion==1 || opcion==2 || opcion==3) {
->>>>>>> branch 'main' of https://github.com/Dnidof/SurvivalVacation.git
 			
-<<<<<<< HEAD
-			else if(opcion==2 && this.getOpcion(3).cumpleRequisitoObjeto()) {
-				this.getOpcion(2).aplicarConsecuencias();
-				seHaRealizado=true;
-			}
-=======
 				if(opcion==1 && this.getOpcion(1).cumpleRequisitoObjeto()) {
 					this.getOpcion(1).aplicarConsecuencias();
 					seHaRealizado=true;
 				}
->>>>>>> branch 'main' of https://github.com/Dnidof/SurvivalVacation.git
 			
-<<<<<<< HEAD
-			else if(opcion==3 && this.getOpcion(3).cumpleRequisitoObjeto()) {
-				this.getOpcion(3).aplicarConsecuencias();
-				seHaRealizado=true;
-			}
-			else
-			{
-				
-=======
 				else if(opcion==2 && this.getOpcion(2).cumpleRequisitoObjeto()) {
 					this.getOpcion(2).aplicarConsecuencias();
 					seHaRealizado=true;
@@ -57,9 +34,9 @@ public class EventoUnico extends Evento{
 			}
 			else {
 				System.out.println("Introduce una opcion valida");
->>>>>>> branch 'main' of https://github.com/Dnidof/SurvivalVacation.git
 			}
 		}
 
 	}
 }
+
