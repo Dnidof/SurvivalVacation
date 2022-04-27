f1e7ad346873be25418acea5c612c23d36ca3e78
imano <imano@DESKTOP-NTMBH2C>
2022-04-27 21:09:12 +0200

    guapísimo funciona

--- src/org/wipgames/survivalvacation/Eventos.java
+++ src/org/wipgames/survivalvacation/Eventos.java
@@ -22,20 +22,20 @@ public class Eventos {
 	public void cargarEventosFichero(String pNombreFichero) {
 		String linea;
 		String dirActual = System.getProperty("user.dir");
-		String dirFicheroCompleto = "C:\\Users\\alberto\\eclipse-workspace\\wipgames\\src\\org\\wipgames\\survivalvacation\\" + pNombreFichero;
+		String dirFicheroCompleto = "C:\\Users\\imano\\git\\SurvivalVacation\\src\\org\\wipgames\\survivalvacation\\" + pNombreFichero;
 		try {
 			InputStream fichero = new FileInputStream(dirFicheroCompleto);
 			Scanner sc = new Scanner(fichero);
 			while(sc.hasNextLine()) {
 				linea = sc.nextLine();
-				String[] parametrosEvento = linea.split("$");
+				String[] parametrosEvento = linea.split("\\$");
 				//id$enunciado$opcion1$opcion2$opcion3 0-4
 				//cada opcion enunciado%salud%hambre%sed%cordura%idConsecuenciaObjeto%cantidad%idObjetoReq%enunciado%victoria 0-9
 				Opcion opcion1,opcion2,opcion3;
 				try {
 					int pSalud,pHambre,pSed,pCordura,pConsecuenciaObjeto,pReqObjeto,pCantidadConsecuenciaObjeto;
 					boolean pOpcionVictoria;
-					String[] opcionString = parametrosEvento[2].split("%");
+					String[] opcionString = parametrosEvento[2].split("\\%");
 					pSalud = Integer.parseInt(opcionString[1]);
 					pHambre = Integer.parseInt(opcionString[2]);
 					pSed = Integer.parseInt(opcionString[3]);
@@ -46,7 +46,7 @@ public class Eventos {
 					pOpcionVictoria = Boolean.parseBoolean(opcionString[9]);
 					opcion1 = new Opcion(opcionString[0],pSalud,pHambre,pSed,pCordura,pConsecuenciaObjeto,opcionString[8],pReqObjeto,pCantidadConsecuenciaObjeto,pOpcionVictoria);
 					
-					opcionString = parametrosEvento[3].split("%");
+					opcionString = parametrosEvento[3].split("\\%");
 					pSalud = Integer.parseInt(opcionString[1]);
 					pHambre = Integer.parseInt(opcionString[2]);
 					pSed = Integer.parseInt(opcionString[3]);
@@ -57,7 +57,7 @@ public class Eventos {
 					pOpcionVictoria = Boolean.parseBoolean(opcionString[9]);
 					opcion2 = new Opcion(opcionString[0],pSalud,pHambre,pSed,pCordura,pConsecuenciaObjeto,opcionString[8],pReqObjeto,pCantidadConsecuenciaObjeto,pOpcionVictoria);
 					
-					opcionString = parametrosEvento[4].split("%");
+					opcionString = parametrosEvento[4].split("\\%");
 					pSalud = Integer.parseInt(opcionString[1]);
 					pHambre = Integer.parseInt(opcionString[2]);
 					pSed = Integer.parseInt(opcionString[3]);
@@ -70,9 +70,9 @@ public class Eventos {
 					
 					this.lista.add(new EventoUnico(Integer.parseInt(parametrosEvento[0]),parametrosEvento[1],opcion1,opcion2,opcion3));
 				} catch(NumberFormatException e) {
-					
+					System.out.println("Se  ha producido una NumberFormatException");
 				} catch(IndexOutOfBoundsException e) {	//Si alguna linea tiene algun error de formato no se añade ese evento y pasamos de línea
-					
+					System.out.println("Se  ha producido una IndexOutOfBoundsException");
 				}		
 			}
 			sc.close();
