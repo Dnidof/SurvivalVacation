25799c306173c11724d58c59b4e9a3ca3c14efcf
Diego <nidofonsecadiego@gmail.com>
2022-04-28 13:03:43 +0200

    Clase test

--- src/org/wipgames/survivalvacation/test.java
+++ src/org/wipgames/survivalvacation/test.java
@@ -1,7 +1,7 @@
 package org.wipgames.survivalvacation;
 
 public class test {
-
+	// Esta clase la voy a utilizar para prepararar los metodos de estadisticas, luego la borramos
 	public static void main(String[] args) {
 		System.out.printf("%-40.30s 1 %n", "obj");
 		System.out.printf("%-40.30s 2", "objetoConNombreMuyLargo");
