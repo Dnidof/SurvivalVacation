7b6892702191794a1eb7d5fa5b7549db2b997a38
imano <imano@DESKTOP-NTMBH2C>
2022-04-29 12:42:59 +0200

    si

--- src/org/wipgames/survivalvacation/Inventario.java
+++ src/org/wipgames/survivalvacation/Inventario.java
@@ -112,19 +112,19 @@ public class Inventario {
     	o = new Objeto("Llave misteriosa",3,0);
     	this.anadirObjeto(o);
     	
-    	o = new Objeto("Palanca",4,0);
+    	o = new Objeto("Linterna",4,0);
     	this.anadirObjeto(o);
     	
-    	o = new Objeto("Espada",5,0);
+    	o = new Objeto("Monedas de oro",5,0);
     	this.anadirObjeto(o);
     	
-    	o = new Objeto("Monedas de oro",6,0);
+    	o = new Objeto("Radio",6,0);
     	this.anadirObjeto(o);
     	
-    	o = new Objeto("Linterna",7,0);
+    	o = new Objeto("espada",7,0);
     	this.anadirObjeto(o);
     	  	
-    	o = new Objeto("Radio",8,0);
+    	o = new Objeto("Palanca",8,0);
     	this.anadirObjeto(o);
     	
     	o = new Objeto("Reliquia Enana",9,0);
@@ -148,26 +148,29 @@ public class Inventario {
     		System.out.println("1-  "  + this.buscarObjetoPorId(0).imprimirNombreObjeto() + "   -10 segundos");
     		System.out.println("2-  "  + this.buscarObjetoPorId(1).imprimirNombreObjeto() + "   -10 segundos");
     		System.out.println("3-  "  + this.buscarObjetoPorId(2).imprimirNombreObjeto() + "   -10 segundos");
-    		if (!this.tieneEsteObjeto(this.buscarObjetoPorId(7))) {
-    			System.out.println("8-  "  + this.buscarObjetoPorId(7).imprimirNombreObjeto() + "   -10 segundos");
+    		if (!this.tieneEsteObjeto(this.buscarObjetoPorId(3))) {
+    			System.out.println("4-  "  + this.buscarObjetoPorId(3).imprimirNombreObjeto() + "   -10 segundos");
     		}
     	}
     	if (pNumSegundos >= 20) {
-    		if (!this.tieneEsteObjeto(this.buscarObjetoPorId(3))) {
-    			System.out.println("4-  "  + this.buscarObjetoPorId(3).imprimirNombreObjeto() + "   -20  segundos");
+    		if (!this.tieneEsteObjeto(this.buscarObjetoPorId(4))) {
+    			System.out.println("5-  "  + this.buscarObjetoPorId(4).imprimirNombreObjeto() + "   -20  segundos");
     		}
     		if (this.tieneEsteObjeto(this.buscarObjetoPorId(5))) {
     			System.out.println("6-  "  + this.buscarObjetoPorId(5).imprimirNombreObjeto() + "   -20  segundos");
     		}
     	}
     	if (pNumSegundos >= 30) {
-    		if (!this.tieneEsteObjeto(this.buscarObjetoPorId(4))) {
-    			System.out.println("5-  "  + this.buscarObjetoPorId(4).imprimirNombreObjeto() + "   -30  segundos");
+    		if (!this.tieneEsteObjeto(this.buscarObjetoPorId(6))) {
+    			System.out.println("7-  "  + this.buscarObjetoPorId(6).imprimirNombreObjeto() + "   -30  segundos");
     		}
     	}
     	if (pNumSegundos >= 40) {
-    		if (!this.tieneEsteObjeto(this.buscarObjetoPorId(6))) {
-    			System.out.println("7-  "  + this.buscarObjetoPorId(6).imprimirNombreObjeto() + "   -40  segundos");
+    		if (!this.tieneEsteObjeto(this.buscarObjetoPorId(7))) {
+    			System.out.println("8-  "  + this.buscarObjetoPorId(7).imprimirNombreObjeto() + "   -40  segundos");
+    		}
+    		if (!this.tieneEsteObjeto(this.buscarObjetoPorId(8))) {
+    			System.out.println("9-  "  + this.buscarObjetoPorId(8).imprimirNombreObjeto() + "   -40  segundos");
     		}
     	}
     }
